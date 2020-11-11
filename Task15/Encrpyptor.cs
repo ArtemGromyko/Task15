729fa545baa79d60fdbe9a91a2d4d68d8f7566be
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-11-11 03:03:24 +0300

    Добавьте файлы проекта.

---
+++ Task15/Encrpyptor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Task15
+{
+    class Encrpyptor
+    {
+        public string KeyContainerName { get; private set; }
+        private CspParameters csp;
+        public readonly UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        public Encrpyptor(string s)
+        {
+            KeyContainerName = s;
+            csp = new CspParameters() { KeyContainerName = KeyContainerName };
+        }
+        public byte[] ToEncrypt(string s)
+        {
+            byte[] dataToEncrypt = ByteConverter.GetBytes(s);
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(csp))
+            {
+                return RSA.Encrypt(dataToEncrypt, false);
+            }
+        }
+        public string ToDecrypt(byte[] data)
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(csp))
+            {
+                return ByteConverter.GetString(RSA.Decrypt(data, false));
+            }
+        }
+    }
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "my data";
+            Encrpyptor enc = new Encrpyptor("MyKeyContainer");
+            byte[] encryptedData = enc.ToEncrypt(data);
+            string s = enc.ByteConverter.GetString(encryptedData);
+            Console.WriteLine(s);
+            s = enc.ToDecrypt(encryptedData);
+            Console.WriteLine(s);
+        }
+    }
+}
